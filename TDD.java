913e0cf898a0fd58b6e0222eb11546d01d39322d
Rahul Kumar <raulk023@gmail.com>
2024-11-06 11:54:50 +0530

    added functions for handling delimeters

--- TDD.java
+++ TDD.java
@@ -14,11 +14,29 @@ public class TDD {
             delimiter = m.group(1);
             numbersWithoutDelimiter = m.group(2);
         }
-        return 0;
+
+        String[] numberStrings = numbersWithoutDelimiter.split(Pattern.quote(delimiter));
+        int[] numbersInt = new int[numberStrings.length];
+        StringBuilder negativeNumbers = new StringBuilder();
+
+        for (int i = 0; i < numberStrings.length; i++) {
+            int number = Integer.parseInt(numberStrings[i].trim());
+            if (number < 0) {
+                negativeNumbers.append(number).append(",");
+            }
+            numbersInt[i] = number;
+        }
+
+        if (negativeNumbers.length() > 0) {
+            throw new IllegalArgumentException(
+                    "negative numbers not allowed: " + negativeNumbers.substring(0, negativeNumbers.length() - 1));
+        }
+
+        return Arrays.stream(numbersInt).sum();
     }
 
     public static void main(String[] args) {
-        int ans = add("");
+        int ans = add("1,2,3");
         System.out.println(ans);
     }
 }
