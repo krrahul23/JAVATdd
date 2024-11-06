58958567e321ea2314e295dfbe61001eb01321aa
Rahul Kumar <raulk023@gmail.com>
2024-11-06 11:53:20 +0530

    added functions

--- TDD.java
+++ TDD.java
@@ -1,9 +1,19 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TDD {
     public static int add(String numbers) {
         if (numbers.isEmpty())
             return 0;
+        String delimiter = ",";
+        String numbersWithoutDelimiter = numbers;
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(numbers);
+        if (m.find()) {
+            delimiter = m.group(1);
+            numbersWithoutDelimiter = m.group(2);
+        }
         return 0;
     }
 
